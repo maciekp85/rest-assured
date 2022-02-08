bb53754307a4d9d2c87d5401f8fbdd43010ba781
Maciej Piotrowski <ext-maciej.piotrowski@finago.com>
2022-02-08 17:49:58 +0100

    Post http method and request and response logging

---
+++ src/test/java/pl/javastart/restasssured/test/http/methods/BasicHttpMethodsTests.java
@@ -0,0 +1,34 @@
+package pl.javastart.restasssured.test.http.methods;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class BasicHttpMethodsTests {
+
+    @Test
+    public void givenPetWhenPostPetThenPetIsCreatedTest() {
+        String pet = "{\n" +
+                "  \"id\": 123,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 1,\n" +
+                "    \"name\": \"dogs\"\n" +
+                "  },\n" +
+                "  \"name\": \"Burek\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"http://photos.com/dog1.jpg\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 1,\n" +
+                "      \"name\": \"dogs-category\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+
+        given().log().all().body(pet).contentType("application/json")
+                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+                .then().log().all().statusCode(200);
+    }
+}
