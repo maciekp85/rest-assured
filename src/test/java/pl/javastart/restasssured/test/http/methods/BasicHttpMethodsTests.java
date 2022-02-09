146d299f325cd26e0ea0f121dfb43f81e98e636b
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2022-02-09 14:16:06 +0100

    Delete method usage

--- src/test/java/pl/javastart/restasssured/test/http/methods/BasicHttpMethodsTests.java
+++ src/test/java/pl/javastart/restasssured/test/http/methods/BasicHttpMethodsTests.java
@@ -89,4 +89,34 @@ public class BasicHttpMethodsTests {
                 .when().put("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
                 .then().log().all().statusCode(200);
     }
+
+    @Test
+    public void givenExistingPetIdWhenDeletingPetThenIsDeletedTest() {
+        String pet = "{\n" +
+                "  \"id\": 445,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 1,\n" +
+                "    \"name\": \"dogs\"\n" +
+                "  },\n" +
+                "  \"name\": \"Burek\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"http://photos.com/dog1.jpg\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 1,\n" +
+                "      \"name\": \"dogs-category\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+
+        given().log().all().body(pet).contentType("application/json")
+                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+                .then().statusCode(200);
+
+        given().log().all().contentType("application/json").pathParam("petId", 445)
+                .when().delete("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/{petId}")
+                .then().log().all().statusCode(200);
+    }
 }
