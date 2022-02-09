470436368f25d575e1a883ecedb27c64e816d421
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2022-02-09 15:25:08 +0100

    Serialization

--- src/test/java/pl/javastart/restasssured/test/http/methods/BasicHttpMethodsTests.java
+++ src/test/java/pl/javastart/restasssured/test/http/methods/BasicHttpMethodsTests.java
@@ -1,6 +1,11 @@
 package pl.javastart.restasssured.test.http.methods;
 
 import org.testng.annotations.Test;
+import pl.javastart.main.pojo.Category;
+import pl.javastart.main.pojo.Pet;
+import pl.javastart.main.pojo.Tag;
+
+import java.util.Collections;
 
 import static io.restassured.RestAssured.given;
 
@@ -8,24 +13,21 @@ public class BasicHttpMethodsTests {
 
     @Test
     public void givenPetWhenPostPetThenPetIsCreatedTest() {
-        String pet = "{\n" +
-                "  \"id\": 123,\n" +
-                "  \"category\": {\n" +
-                "    \"id\": 1,\n" +
-                "    \"name\": \"dogs\"\n" +
-                "  },\n" +
-                "  \"name\": \"Burek\",\n" +
-                "  \"photoUrls\": [\n" +
-                "    \"http://photos.com/dog1.jpg\"\n" +
-                "  ],\n" +
-                "  \"tags\": [\n" +
-                "    {\n" +
-                "      \"id\": 1,\n" +
-                "      \"name\": \"dogs-category\"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"status\": \"available\"\n" +
-                "}";
+
+        Category category = new Category();
+        category.setId(1);
+        category.setName("dogs");
+
+        Tag tag = new Tag();
+        tag.setId(1);
+        tag.setName("dogs-category");
+
+        Pet pet = new Pet();
+        pet.setId(123);
+        pet.setCategory(category);
+        pet.setPhotoUrls(Collections.singletonList("http://photos.com/dog1.jpg"));
+        pet.setTags(Collections.singletonList(tag));
+        pet.setStatus("available");
 
         given().log().all().body(pet).contentType("application/json")
                 .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
