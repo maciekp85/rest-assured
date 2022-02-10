5cc4250800582beb229e23af0d77fe9ae0f2cdb6
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2022-02-10 21:21:16 +0100

    Issues related with SSL certificates

---
+++ src/test/java/pl/javastart/restasssured/test/ssl/BadSslTest.java
@@ -0,0 +1,13 @@
+package pl.javastart.restasssured.test.ssl;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class BadSslTest {
+
+    @Test
+    public void sslCertExpiredTest() {
+        given().relaxedHTTPSValidation().when().get("https://expired.badssl.com/").then().statusCode(200);
+    }
+}
