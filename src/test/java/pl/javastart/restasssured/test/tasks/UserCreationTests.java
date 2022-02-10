99ea4511559cd4fbc5ee4517bf52655379cbc2e6
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2022-02-10 12:58:30 +0100

    Exercise 5.10

--- src/test/java/pl/javastart/restasssured/test/tasks/UserCreationTests.java
+++ src/test/java/pl/javastart/restasssured/test/tasks/UserCreationTests.java
@@ -5,7 +5,7 @@ import pl.javastart.main.pojo.User;
 
 import static io.restassured.RestAssured.given;
 
-public class UserCreationTests {
+public class UserCreationTests extends TestBase {
 
     @Test
     public void givenCorrectUserDataWhenCreateUserThenUserIsCreatedTest() {
@@ -22,13 +22,13 @@ public class UserCreationTests {
         given().log().all()
                 .contentType("application/json")
                 .body(user)
-                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/user")
+                .when().post("user")
                 .then().log().all().statusCode(200);
 
         given().log().all()
                 .contentType("application/json")
                 .pathParam("username", user.getUsername())
-                .when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}")
+                .when().get("user/{username}")
                 .then().log().all().statusCode(200);
     }
 }
--- src/test/java/pl/javastart/restasssured/test/tasks/UserUpdateTests.java
+++ src/test/java/pl/javastart/restasssured/test/tasks/UserUpdateTests.java
@@ -5,10 +5,10 @@ import pl.javastart.main.pojo.User;
 
 import static io.restassured.RestAssured.given;
 
-public class UserUpdateTests {
+public class UserUpdateTests extends TestBase {
 
     @Test
-    public void givenCorectUserDataWhenFirstNameLastNameAreUpdatedThenUserDataIsUpdatedTest() {
+    public void givenCorrectUserDataWhenFirstNameLastNameAreUpdatedThenUserDataIsUpdatedTest() {
         User user = new User();
         user.setId(445);
         user.setUsername("firstuser");
@@ -22,7 +22,7 @@ public class UserUpdateTests {
         given().log().all()
                 .contentType("application/json")
                 .body(user)
-                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/user")
+                .when().post("user")
                 .then().log().all().statusCode(200);
 
         user.setFirstName("Maciek");
@@ -32,13 +32,13 @@ public class UserUpdateTests {
                 .contentType("application/json")
                 .pathParam("username", user.getUsername())
                 .body(user)
-                .when().put("https://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}")
+                .when().put("user/{username}")
                 .then().log().all().statusCode(200);
 
         given().log().all()
                 .contentType("application/json")
                 .pathParam("username", user.getUsername())
-                .when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}")
+                .when().get("user/{username}")
                 .then().log().all().statusCode(200);
     }
 }
