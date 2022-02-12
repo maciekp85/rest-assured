ccbd16e948e126fa3ca5e2dce7647612d42cf0fb
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2022-02-12 17:54:51 +0100

    Exercise 7.1

--- src/test/java/pl/javastart/restasssured/test/tasks/TestBase.java
+++ src/test/java/pl/javastart/restasssured/test/tasks/TestBase.java
@@ -4,13 +4,16 @@ import io.restassured.RestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class TestBase {
 
+    @Parameters({"baseUri", "basePath"})
     @BeforeClass
-    public void setupConfiguration() {
-        RestAssured.baseURI = "https://swaggerpetstore.przyklady.javastart.pl/";
-        RestAssured.basePath = "v2";
+    public void setupConfiguration(@Optional("https://swaggerpetstore.przyklady.javastart.pl") String baseUri, @Optional("v2") String basePath) {
+        RestAssured.baseURI = baseUri;
+        RestAssured.basePath = basePath;
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
     }
 }
